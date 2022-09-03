e1a1641837399b7969ac30ef6fba81c3ab78f476
Samir Musaev <tp050965@mail.apu.edu.my>
2022-09-03 23:11:54 +0800

    init repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace farm2plate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
